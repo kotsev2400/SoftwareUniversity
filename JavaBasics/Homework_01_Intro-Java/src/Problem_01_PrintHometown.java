0ee879cb3eca00d1e8b2aa534989a4ac04f2804c
kotsev2400 <georgikotsev2400@gmail.com>
2015-04-16 15:46:41 +0300

    Remove duplicated directory

--- JavaBasics/Homework_01_Intro-Java/src/Problem_01_PrintHometown.java
+++
@@ -1,7 +0,0 @@
-
-public class Problem_01_PrintHometown {
-	public static void main(String[] args) {
-		String HomeTown = "Samokov";
-		System.out.println(HomeTown);
-	}
-}
--- JavaBasics/Homework_01_Intro-Java/src/Problem_02_CurrentDateTime.java
+++
@@ -1,9 +0,0 @@
-import java.util.Date;
-
-
-public class Problem_02_CurrentDateTime {
-	public static void main(String[] args) {
-		Date CurrentDate = new Date();
-		System.out.println(CurrentDate);
-	}
-}
