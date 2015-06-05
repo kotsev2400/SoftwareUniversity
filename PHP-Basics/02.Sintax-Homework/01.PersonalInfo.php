c506d4ffbf15a6e1505b67fb250b6ef095f36c3e
kotsev2400 <georgikotsev2400@gmail.com>
2015-06-05 15:30:33 +0300

    remove

--- PHP-Basics/02.Sintax-Homework/01.PersonalInfo.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$firstName = "Georgi";
-$secondName = "Ivanov";
-$age = "26";
-
-echo "My name is " . $firstName . " " . $secondName . " and I am " . $age . " years old.";
--- PHP-Basics/02.Sintax-Homework/02.SumTwoNumbers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$firstNumber = 1.567808;
-$secondNumber = 2.988;
-
-$sum = $firstNumber + $secondNumber;
-echo number_format($sum, 2);
